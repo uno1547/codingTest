8ce519cfacbddb990a0d76883c31e5ee0c52d8d9
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-08-22 15:03:09 +0900

    algorithm - selection, bubble, insertion sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -16,7 +16,9 @@ function bubbleSort(arr) { //성공! 다르긴하지만 틀린듯
 }
 
 const arr = [9, 8, 2, 4, 3]
+// DB의 코드
 function bubbleSort2(arr) {
+  // i = 4; i > 0
   for (let i = arr.length - 1; i > 0; i--) { //  i = 4,3,2,1 for문돌릴때 반복변수유연하게 쓸수있다.
     for (let j = 0; j < i; j++) {            // 각단계별로 마지막에 정렬된원소의인덱스
       if (arr[j] > arr[j + 1]) {             // j = 0
@@ -28,7 +30,7 @@ function bubbleSort2(arr) {
   }
   console.log(arr)
 }
-bubbleSort2(arr)
+// bubbleSort2(arr)
 // 24/3/21 
 // 어떤 글에는 정렬되어있는 상태에선 시간복잡도가 O(N)이라는데 이게 뭔말임 break를 쓰면 비교연산 안해서 outer루프만 돈다는얘긴가
 function bubbleSort(arr) {
@@ -45,8 +47,57 @@ function bubbleSort(arr) {
     // console.log(arr);
   }
 }
-startTime = new Date().getTime()
-// console.log(startTime);
 
-endTime = new Date().getTime()
-console.log(endTime - startTime);
+
+// 24/8/21
+// 버블 정렬 시간복잡도 O(N^2)
+// arr : [9, 8, 2, 4, 3]
+/*
+1단계(i = 0)
+8 9 2 4 3
+8 2 9 4 3
+8 2 4 9 3 
+8 2 4 3 *9
+2단계
+2 8 4 3 9
+2 4 8 3 9
+2 4 3 *8 *9
+3단계
+2 4 3 8 9
+2 3 *4 *8 *9
+4단계
+2 *3 *4 *8 *9
+5단계 
+끝
+*/
+// 단계랑 연산횟수따져볼려면 가장쉽게 len = 3인 배열을 생각하자
+/*
+[2,3,1]
+1단계
+*/
+
+function bubbleSort(arr) {
+  for(let i = 0; i < arr.length-1; i++) { // 0 1 2 3 (4번)
+    for(let j = 0; j < arr.length - 1 - i ; j++) { // 0 ~ 4번
+      console.log('i번째반복',i, '인덱스',j);
+      console.log('인접원소', arr[j], arr[j+1]);
+      if(arr[j] > arr[j+1]) [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      console.log(arr);
+    }
+  }
+  // console.log(arr);
+}
+// bubbleSort(arr)
+
+// 이해하기 쉬움 이게 제일??
+function bubbleSort2(arr) {
+  for(let i = 1; i <= arr.length - 1; i++) { //단계에 기준으로 설정 ex) 길이 5인 배열 > 1 2 3 4 단계
+    for(let j = 0; j <= arr.length - 1 - i; j++) { //인덱스를 기준 ex) 1단계 인덱스 0~3 2단계 인덱스 0~2
+      console.log(`${i}단계, 인덱스 ${j}, ${j+1}`);
+      // console.log('인접원소', arr[j], arr[j+1]);
+      if(arr[j] > arr[j+1]) [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      console.log(arr);
+    }
+  }
+}
+bubbleSort2(arr)
--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -1,4 +1,4 @@
-const arr = [2,4,3,1,9]
+const arr = [2,4,3,1,5]
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) { //i = 1,2,3,4,5 단계
     for (let j = i; j > 0; j--) { 
@@ -14,4 +14,50 @@ function insertionSort(arr) {
   }
   console.log(arr)
 }
-insertionSort(arr)
+// insertionSort(arr)
+
+// 24 / 8 / 21
+// 삽입정렬 시간복잡도 O(N^2)
+// 일반적인경우에 선택정렬, 버블정렬 보다 빠르게 동작한다고 함
+// 각 단계에서 원소가 들어갈 위치를 찾는다(위치가 나올때까지 왼쪽으로 이동)
+// 1단계에서 제일 왼쪽원소는 정렬이 되어있다고 가정한다. (왜냐 오른쪽 원소들은 왼쪽을 살펴서 자리를 찾기대문!!)
+// [2, 4, 3, 1, 5]
+/*
+*2 4 3 1 5
+1단계
+j = 1 (arr[1] < arr[0] 아니므로 break) 이 break덕에 작게나마 빨라짐
+*2 *4 3 1 5
+2단계
+j = 2 (arr[2] < arr[1] 이므로 스왑)
+*2 3 *4 1 5
+J = 1 (arr[1] < arr[0] 아니므로 break)
+*2 *3 *4 1 5
+3단계
+j = 3 (arr[3] < arr[2] 이므로 스왑)
+*2 *3 1 *4 5
+j = 2 (arr[2] < arr[1] 이므로 스왑)
+*2 1 *3 *4 5
+j = 1 (arr[1] < arr[0] 이므로 스왑)
+*1 *2 *3 *4 5
+4단계(면 끝남)
+*1 *2 *3 *4 *5
+단계별로 특정원소가 본인기준 왼쪽을 살피며 들어갈 자리를 살핌 
+*/
+function insertionSort1(arr) {
+  for(let i = 1; i < arr.length; i++) { // 반복변수 : 단계
+    for(let j = i; j > 0; j--) { // 0까지 갈필요가 없느게, 시작할때 0번째 원소는 정렬된것이라고 가정했음
+      // console.log(`${i}번째원소로 ${j}부터 0까지 찾는중`);
+      // if(arr[i] < arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]] // 이러면 스왑후에 arr[i]값이 바뀌게돼서 뒤죽박죽되는듯
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+      } else {
+        break
+      }
+    }
+  }
+  console.log(arr);
+}
+insertionSort1(arr)
+
+//// 어떤 글에는 정렬되어있는 상태에선 시간복잡도가 O(N)이라는데 이게 뭔말임 break를 쓰면 비교연산 안해서 outer루프만 돈다는얘긴가
+// 최악의 경우에는 다른 정렬(삽입, 선택) 처럼 O(N^2) 이지만 최선의 경우에는 O(N)에 가까운 복잡도를 가질수있다.
