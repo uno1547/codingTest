4851e6f667d4e51983bd7962f0b8127498782262
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-09-02 01:52:54 +0900

    PS-DFS
    BOJ 2606

--- DFS/dfs.js
+++ DFS/dfs.js
@@ -25,5 +25,84 @@ function dfs(graph, v, visited) { // 그래프, 현재노드, 방문처리배열
   }
   // console.log(`${v}번노드의 인접노드 탐색완료!`);
 }
-dfs(graph, 0, visited)
+// dfs(graph, 0, visited) // 재귀함수로 
 // 스택으로는 어떻게하져
+
+
+/*
+1. 시작노드 방문처리
+2. 시작노드의 인접노드 중 방문하지않은 인접노드확인
+3. 2번의 인접노드들에 대해서 다시 과정 반복
+*/
+
+const graph2 = [
+  [], //0번노드
+  [2, 3, 4], //1번노드 
+  [1, 5], //2번노드
+  [1, 6, 7], //3번노드
+  [1, 8, 9], //4번노드
+  [2], //5번노드
+  [3], //6번노드
+  [3], //7번노드
+  [4], //8번노드
+  [4], //9번노드
+]
+
+function dfs2(graph, v, visited) {
+  visited[v] = true // 방문처리
+
+  console.log(`${v}번 방문처리`);
+  //인접노드 확인
+  for(let node of graph[v]) {
+    if(visited[node] == false) { //인접노드가 방문하지않았다면 방문
+      dfs2(graph, node, visited)
+    }
+  }
+}
+const visited2 = new Array(10).fill(false)
+
+dfs2(graph2, 1, visited2)
+
+// 스택을 이용한 DFS 구현
+/*
+1. 스택에 시작노드 push
+2. 스택에서 pop, 방문처리
+3. 방문처리한 노드의 인접노드(방문안한) 있다면 스택에 push
+4. 스택의 길이가 0이 될때가지 2,3 번 과정을 반복한다.
+*/
+
+// 동일한 그래프에 대해서 스택DFS구현해보겟다.
+const graph3 = [
+  [], //0번노드
+  [2, 3, 4], //1번노드 
+  [1, 5], //2번노드
+  [1, 6, 7], //3번노드
+  [1, 8, 9], //4번노드
+  [2], //5번노드
+  [3], //6번노드
+  [3], //7번노드
+  [4], //8번노드
+  [4], //9번노드
+]
+const visited3 = new Array(10).fill(false)
+function dfs3(graph, v, visited) {
+  const stack = []
+  stack.push(v) // 1번 push
+  console.log('시작', stack);
+
+  while(stack.length != 0) {
+    const v = stack.pop() // 꺼내고, 방문안한 노드면 방문표시
+    if (visited[v] == false) {
+      visited[v] = true
+      console.log(`${v}번 노드 방문`);
+
+      for(let adj of graph[v]) {
+        if (visited[adj] == false) {
+          stack.push(adj)
+        }
+      }
+      console.log(stack);
+    }
+  }
+}
+dfs3(graph3, 1, visited3)
--- DFS/dfs1.js
+++ DFS/dfs1.js
@@ -1,6 +1,53 @@
 //2606 바이러스
 let fs = require('fs')
 let input = fs.readFileSync('../input.txt').toString().trim().split('\n')
+let [num, connectNum, ...connects] = input
+num = Number(num)
+connectNum = Number(connectNum)
+connects = connects.map((connect) => connect.split(' ').map(Number))
+console.log('connects', connects);
+
+let network = new Array(num + 1).fill(1).map(() => new Set)
+connects.forEach(([start, end], idx) => {
+  console.log(`start${start} end${end}`);
+  network[start].add(end)
+  network[end].add(start)
+})
+console.log(network);
+network = network.map((set) => Array.from(set))
+console.log(network);
+
+const infected = new Array(num + 1).fill(false)
+
+function search(network, i, infected) {
+  infected[i] = true
+
+  for(let node of network[i]) {
+    if(infected[node] == false) search(network, node, infected)
+  }
+}
+search(network, 1, infected)
+
+console.log(infected);
+
+const ans = infected.reduce((num, val) => {
+  if(val) num++
+  return num 
+}, 0)
+
+console.log(ans);
+
+
+
+
+
+
+
+
+
+
+
+/*
 let [num, setNum, ...connects] = input
 num = Number(num)
 setNum = Number(setNum)
@@ -15,10 +62,10 @@ connects.forEach(([from, to]) => {
 
 function DFS(com) {
   visited[com] = true
-  //ans += 1 인지 모르겟네
+  //ans += 1 인지 모르겟네 1번제외 몇개인지 세는거니깐 ㄴㄴ 그게아니라
   for(let n of graph[com]) {
     if (!visited[n]) { //인접컴퓨터 방문안했으면
-      ans += 1 //ans위치가 여기인지 
+      // ans += 1 //ans위치가 여기인지 
       DFS(n)
     }
   }
@@ -26,3 +73,4 @@ function DFS(com) {
 DFS(1)
 console.log(ans); // 여기서 1빼야되는거임? 안빼도 맞았는데 
 // 이 인접리스트를 탐색하여, 1번부터 방문, 
+*/
